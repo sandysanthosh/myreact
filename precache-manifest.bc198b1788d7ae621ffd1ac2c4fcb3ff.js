b8517df65434d76d6dd419bf2e999680a16e65fd
sandysanthosh <sandysanthosh101@gmail.com>
2020-05-30 11:04:30 +0530

    Updates

---
+++ precache-manifest.bc198b1788d7ae621ffd1ac2c4fcb3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a34541a9c4107a50928828a640e41b",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "8250de649bea4afccde1",
+    "url": "/myreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f81867e0a461438ad9b6",
+    "url": "/myreact/static/js/2.4ef118ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.4ef118ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8250de649bea4afccde1",
+    "url": "/myreact/static/js/main.0ad0a012.chunk.js"
+  },
+  {
+    "revision": "de177c3958236aa05f51",
+    "url": "/myreact/static/js/runtime-main.f3abf660.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
